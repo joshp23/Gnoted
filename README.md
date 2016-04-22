102b9ca49018a33b6b26870d5741285e3c06f9b6
joshp23 <joshp@nousessence.com>
2016-04-22 03:02:07 -0400

    Added files via upload

---
+++ README.md
@@ -0,0 +1,77 @@
+Noted
+======
+by Josh Panter: http://unfettered.net
+
+A secure PHP based Web App for reading either Gnote or Tomboy Notes.
+
+###TODO (in almost no particular order): 
+
+1. Move all config variables to a single file and add accessible user customization
+2. Add arbitrary note sharing via url with and/or without auth (ie., anonymous users can still see notes if they have direct url access)
+3. Add built in url shortenr for above feature
+4. Add honeypot options (see project honeypot https://www.projecthoneypot.org/)
+5. Add notebooks
+6. Add translations
+7. Add ability to sort notes via name and date of creation and/or last edit
+8. Exclude template files
+
+###SETUP - this setup assumes that your webserver is apache
+
+1. Copy the Noted main folder (this repository) to your web server. See the provided alias example (noted.conf) to put this in the web path.
+
+2. Get Gnote/Tomboy noted into the path of Noted. There are two ways to do this.
+
+	2a. Link Tomboy Notes' or Gnote's actual note folder as a folder named 'notes' to main folder. If you do this.
+
+For Gnote this will be ` ~/.local/share/gnote `
+For Tomboy this will be ` ~/.local/share/tomboy `
+
+```bash
+$ cd /path/to/noted
+$ ln -s ~/.local/share/gnote notes
+```
+
+	2b. Open "assets/noted.php" in your favorite text editor and alter the $APP_PATH setting on line 04 from "notes" to "/home/YOUR_USER_NAME/.local/share/gnote" (or tomboy, respectively).
+
+3. Make certain that www-data is a member of the YOUR_USERNAME group, and then check your work by executing:
+
+```bash
+$ usermod -a -G YOUR_USERNAME www-data
+$ groups www-data
+```
+
+4. Make certain that 750 permissions are set on  ` ~./.local`  ` ~./.local/share` and ` ./.local/share/gnote`
+ 
+```bash
+$ chmod 750 ~./.local
+$ chmod 750 ~./.local/share
+$ chmod 750 ~./.local/share/gnote -R
+```
+5. Set optional variables in assets/noted.php
+
+	a. set auth to true (and follow further instructions in that file) if you want to secure your Noted Web App
+	b. add a personal greeting
+
+###CREDITS
+Scripts used for inspiration and/or copypasta: 
+1. https://wiki.gnome.org/Apps/Tomboy/UsageIdeas
+2. https://github.com/gojigeje/tomboy-php
+3. http://www.zubrag.com/scripts/
+
+feedback is always welcome.
+===========================
+
+    Copyright (C) 2016 Josh Panter
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
---
+++ index.php
@@ -0,0 +1,109 @@
+<?php
+include ("assets/noted.php");
+if ($require_auth == "true") {
+require ("assets/gatekeeper.php");
+  }
+if ($_REQUEST["note"] == "") {
+   $active = "active";
+  }
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/note.png">
+
+    <title>Noted</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-/x/+iIbAU4qS3UecK7KIjLZdUilKKCjUFVdwFx8ba7S/WvxbrYeQuKEt0n/HWqTx" crossorigin="anonymous">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/offcanvas.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href=".">Noted</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="<?=$active?>"><a href=".">Home</a></li>
+            <li class="divider"></li>
+            <li><a target="_blank" href="//wiki.gnome.org/Apps/Gnote/">Gnote</a></li>
+            <li><a target="_blank" href="//wiki.gnome.org/Apps/Tomboy/">Tomboy Notes</a></li>
+          </ul>
+	   <ul class="nav navbar-nav navbar-right">
+            <li><a href="?logout=1">Logout</a></li>
+          </ul>
+        </div><!-- /.nav-collapse -->
+      </div><!-- /.container -->
+    </div><!-- /.navbar -->
+
+    <div class="container">
+
+      <div class="row row-offcanvas row-offcanvas-right">
+        
+        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+          <div class="list-group">
+            <a class="list-group-item active" href="#"><b>Notes :</b></a>
+            <?php foreach($notes as $note) { ?>
+                <a class="list-group-item" href=".?note=<?=$note["id"]?>"><b><?=$note["content"]["title"];?></b></a>
+            <?php } ?>
+          </div>
+        </div><!--/span-->
+
+        <div class="col-xs-12 col-sm-9">
+          <p class="pull-left visible-xs">
+            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle List</button>
+          </p>
+
+          <?php if ($_REQUEST["note"] == ""): ?>
+             <div class="jumbotron">
+               <h2 style="text-decoration: underline;">Noted</h2>
+               <p>Welcome to Noted, the secure, web based note reader for Gnote and Tomboy desktop notes.</p>
+               <p>To get started, simply click on the title of a Note on the left to display its contents.</p>
+             </div>
+          <?php else:
+            $notes = getNotes();
+            $note = $notes[$_REQUEST["note"]];
+            $note["content"] = getNote($note["id"], $note["rev"], true); ?>
+
+            <div class="row">
+               <div class="well col-12">
+                  <?php echo $note["content"]["text"];?>
+               </div>
+            </div><!--/row-->
+
+          <?php endif ?>
+
+        </div><!--/span-->
+
+      </div><!--/row-->
+
+      <hr>
+
+      <footer>
+        <p>&copy; 2016 <a target="_blank" href="//unfettered.net">Unfettered</a></p>
+      </footer>
+
+    </div><!--/.container-->
+
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document for faster page loads-->
+    <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script src="assets/offcanvas.js"></script>
+  </body>
+</html>
