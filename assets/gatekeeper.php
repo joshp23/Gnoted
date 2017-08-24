ee475e41f8a0fd53c1780e81850db008b5394a2b
Josh Panter <joshu@unfettered.net>
2017-08-24 02:31:53 -0400

    Update gatekeeper.php

--- assets/gatekeeper.php
+++ assets/gatekeeper.php
@@ -1,29 +1,6 @@
 <?php
 session_start();
 
-### OPTIONS
-
-// Enter login/password pairs. 
-// Alternatively, set single terms for password only auth.
-$LOGIN_INFORMATION = array(
-  'username' => 'password'
-);
-
-// Set to false for password only auth.
-define('USE_USERNAME', true);
-
-// User will be redirected to this page after logout
-define('LOGOUT_URL', 'http:example.com/');
-
-// Time out after NN minutes of inactivity. Set to 0 to ignore timeout.
-define('TIMEOUT_MINUTES', 30);
-
-// If TIMEOUT_MINUTES is not zero:
-// Set to true to record timeout time from last activity, false from login.
-define('TIMEOUT_CHECK_ACTIVITY', true);
-
-### FUNCTIONS
-
 // Convert timeout to seconds
 $timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
 
