06d5be98a8caf98347d5f8efe4df3b01ebba3c60
Josh Panter <joshu@unfettered.net>
2017-08-25 22:54:07 -0400

    multiple fixes and feature
    -  Themes
    -  Notebooks!
    -  Ordered notes
    -  Cleaned up menus

--- index.php
+++ index.php
@@ -1,112 +1,174 @@
 <?php
 require ("config.php");
 require ("assets/gnoted.php");
-
-if(isset($_REQUEST["note"]) && $_REQUEST["note"] == "") {
-   $active = "active";
-  }
+if ($require_auth == true) require ("assets/gatekeeper.php");
+$inverse = array("slate", "spacelab");
+if( in_array( $THEME, $inverse ) ) {
+	$navbar = null;
+} else {
+	$navbar = "navbar-inverse";
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="assets/note.png">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<link rel="shortcut icon" href="assets/note.png">
 
-    <title>Gnoted</title>
+		<title>Gnoted</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/readable/bootstrap.min.css" rel="stylesheet" integrity="sha384-/x/+iIbAU4qS3UecK7KIjLZdUilKKCjUFVdwFx8ba7S/WvxbrYeQuKEt0n/HWqTx" crossorigin="anonymous">
+		<!-- Bootstrap core CSS -->
+		<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/<?=$THEME;?>/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
 
-    <!-- Custom styles for this template -->
-    <link href="assets/offcanvas.css" rel="stylesheet">
-  </head>
+		<!-- Custom styles for this template -->
+		<link href="assets/offcanvas.css" rel="stylesheet">
+	</head>
 
-  <body>
-    <div class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href=".">Gnoted</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="<?=$active?>"><a href=".">Home</a></li>
-            <li class="divider"></li>
-            <li><a target="_blank" href="//wiki.gnome.org/Apps/Gnote/">Gnote</a></li>
-            <li><a target="_blank" href="//wiki.gnome.org/Apps/Tomboy/">Tomboy Notes</a></li>
-          </ul>
-	  <?php if ($require_auth == true) echo '
-	   <ul class="nav navbar-nav navbar-right">
-            <li><a href="?logout=1">Logout</a></li>
-          </ul>';
-	  ?>
-        </div><!-- /.nav-collapse -->
-      </div><!-- /.container -->
-    </div><!-- /.navbar -->
+	<body>
+		<div class="navbar navbar-fixed-top <?=$navbar;?>" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href=".">Gnoted</a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Notebooks<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+							<?php foreach($tags as $tag) { ?>
+							<li><a href=".?notebook=<?=$tag;?>" id=<?=$tag;?>><?=$tag;?></a></li>
+							<?php } ?>
+							</ul>
+						</li>
+					</ul>
 
-    <div class="container">
+					<ul class="nav navbar-nav navbar-right">
+<li class="dropdown">
+						<li>
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Companion Apps<span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a target="_blank" href="//wiki.gnome.org/Apps/Gnote/" data-toggle="tooltip" title="Linux" >Gnote</a></li>
+								<li class="divider"></li>
+								<li><a target="_blank" href="//wiki.gnome.org/Apps/Tomboy/" data-toggle="tooltip" title="Windows, MacOS, Linux">Tomboy Notes</a></li>
+								<li class="divider"></li>
+								<li><a target="_blank" href="https://play.google.com/store/apps/details?id=org.tomdroid" data-toggle="tooltip" title="Android">Tomdroid Notes</a></li>
+							</ul>
+						</li>
+						<?php if ($require_auth == true) echo '<li><a href="?logout=1">Logout</a></li>'; ?>
+					</ul>
+				</div><!-- /.nav-collapse -->
+			</div><!-- /.container -->
+		</div><!-- /.navbar -->
 
-      <div class="row row-offcanvas row-offcanvas-right">
-        
-        <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
-          <div class="list-group">
-            <a class="list-group-item active" href="#"><b>Notes :</b></a>
-            <?php foreach($notes as $note) { ?>
-                <a class="list-group-item" href=".?note=<?=$note["id"]?>"><b><?=$note["content"]["title"];?></b></a>
-            <?php } ?>
-          </div>
-        </div><!--/span-->
+		<div class="container">
 
-        <div class="col-xs-12 col-sm-9">
-          <p class="pull-left visible-xs">
-            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle List</button>
-          </p>
+			<div class="row row-offcanvas row-offcanvas-right">
 
-          <?php if(!isset($_REQUEST["note"]) ): ?>
-             <div class="jumbotron">
-               <h2 style="text-decoration: underline;">Gnoted</h2>
-               <p>Welcome to Gnoted, the secure, web based note reader for Gnote and Tomboy desktop notes.</p>
-               <p>To get started, simply click on the title of a Note on the left to display its contents.</p>
-             </div>
-          <?php else:
-            $notes = getNotes();
-            if(isset($_REQUEST["note"]) && $_REQUEST["note"] !== null) {
-		$note = $notes[$_REQUEST["note"]];
-            	$note["content"] = getNote($note["id"], $note["rev"], true);
-	    } ?>
+				<div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+					<div class="list-group">
 
-            <div class="row">
-               <div class="well col-12">
-                  <?php if(isset($_REQUEST["note"]) && $_REQUEST["note"] !== null) echo $note["content"]["text"];?>
-               </div>
-            </div><!--/row-->
+						<?php
+						if(isset($_REQUEST["notebook"]) ) { 
+							$notebook = $_REQUEST["notebook"];
+						?>
+						<a class="list-group-item active" ><b><?=$notebook?> Notes</b></a>
+						<?php
+							foreach($notes as $note) {
+								if( $note["content"]["tag"] == $notebook ) {
+						?>
+						<a class="list-group-item" href=".?notebook=<?=$notebook;?>&note=<?=$note["id"]?>"><b><?=$note["content"]["title"];?></b></a>
+						<?php			
+								}
+							}
+						} else { 
+						?>
+						<a class="list-group-item active" ><b>All Notes</b></a>
+						<?php
+							foreach($notes as $note) { 
+								if( $note["content"]["tag"] !== "template" ) { 
+						?>
+						<a class="list-group-item" href=".?note=<?=$note["id"]?>"><b><?=$note["content"]["title"];?></b></a>
+						<?php
+								}
+							}
+						} 
+						?>
 
-          <?php endif ?>
+					</div>
+				</div><!--/span-->
 
-        </div><!--/span-->
+				<div class="col-xs-12 col-sm-9">
+					<p class="pull-left visible-xs">
+						<button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle List</button>
+					</p>
 
-      </div><!--/row-->
+					<?php
+						if(!isset($_REQUEST["note"]) && !isset($_REQUEST["notebook"])) :
+					?>
+					<div class="jumbotron">
+						<h2 style="text-decoration: underline;">Gnoted</h2>
+						<p>Welcome to Gnoted, the secure, web based note reader for Gnote and Tomboy desktop notes.</p>
+						<p>To get started, either click on the title of a Note to display its contents or select a Notebook from the dropdown menu.</p>
+						<p>When viewing  note you can click on its Notebook button to view only notes in that Notebook. If the Notebook has already been selected and you click on an individual Note's Notebook button, you will stay in the Notebook, but willclear that Note.</p>
+						<p>Click on the "Gnoted" menu button to clear the Notebook selection and display all Notes.</p>
+					</div>
 
-      <hr>
+					<?php 
+						elseif(!isset($_REQUEST["note"]) && isset($_REQUEST["notebook"])):
+?>
+					<div class="jumbotron">
+						<h2 style="text-decoration: underline;">Notebook: <?=$_REQUEST["notebook"];?></h2>
+						<p>Click on the title of a Note to display its contents.</p>
+						<p>Click on the "Gnoted" menu button to clear the Notebook selection and display all Notes.</p>
+					</div>
+					<?php
+						else:
+							$notes = getNotes();
+						if(isset($_REQUEST["note"]) && $_REQUEST["note"] !== null) {
+							$note = $notes[$_REQUEST["note"]];
+							$note["content"] = getNote($note["id"], $note["rev"], true);
+						}
+					?>
 
-      <footer>
-        <p>&copy; 2017 <a target="_blank" href="http://unfettered.net">Unfettered</a></p>
-      </footer>
+					<div class="row">
+						<div class="well col-12">
+							<?php 
+								if(isset($_REQUEST["note"]) && $_REQUEST["note"] !== null && isset($_REQUEST["notebook"])) {
+									echo $note["content"]["text"]."<hr><a href='.?notebook=".$note["content"]["tag"]."' class='btn btn-info btn-xs'>".$note["content"]["tag"]."</a>";
+								} elseif(isset($_REQUEST["note"]) && $_REQUEST["note"] !== null) {
+									echo $note["content"]["text"]."<hr><a href='.?notebook=".$note["content"]["tag"]."&note=".$note["id"]."' class='btn btn-info btn-xs'>".$note["content"]["tag"]."</a>";
+								}
+							?>
+						</div>
+					</div><!--/row-->
 
-    </div><!--/.container-->
+							<?php endif ?>
 
-    <!-- Bootstrap core JavaScript -->
-    <!-- Placed at the end of the document for faster page loads-->
-    <script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-    <script src="assets/offcanvas.js"></script>
-  </body>
+				</div><!--/span-->
+
+			</div><!--/row-->
+
+			<hr>
+
+			<footer>
+				<p>&copy; 2017 <a target="_blank" href="https://unfettered.net">Unfettered</a> | <a target="_blank" href="https://github.com/joshp23/Gnoted">Gnoted Home</a></p>
+			</footer>
+
+		</div><!--/.container-->
+
+		<!-- Bootstrap core JavaScript -->
+		<!-- Placed at the end of the document for faster page loads-->
+		<script src="https://code.jquery.com/jquery-1.12.3.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<script src="assets/offcanvas.js"></script>
+
+	</body>
 </html>
